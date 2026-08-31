49973b0fae2e34e3d3bc2bd3df116d5aedff3b4f
KillerGrowth <notifications@killergrowth.com>
2026-08-31 10:08:20 -0500

    fix: swap CARTO tiles for OSM on auctions map (CARTO free tier killed)

--- generate-auctions.mjs
+++ generate-auctions.mjs
@@ -1052,8 +1052,8 @@ function goldIconActive() {
 function initMap() {
   if (map) return;
   map = L.map('sg-map', { zoomControl:true, scrollWheelZoom:false });
-  L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-    attribution:'&copy; <a href="https://carto.com/">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OSM</a>',
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a>',
     subdomains:'abcd', maxZoom:19
   }).addTo(map);
 
